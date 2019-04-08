020829d4f86a587e4bf7f05ae8c5ba601f657152
rajkottam1 <raj.kottam@gmail.com>
2019-04-08 18:37:27 +0530

    Update vpc.tf
    removed  provider

--- vpc.tf
+++ vpc.tf
@@ -1,11 +1,3 @@
-provider "aws" {
-  #access_key = "${var.aws_access_key}"
-  #secret_key = "${var.aws_secret_key}"
-  access_key = ""
-   secret_key = ""
-  #shared_credentials_file = "${var.credentialsfile}"
-  region     = "${var.region}"
-}
 resource "aws_vpc" "terraformmain" {
     cidr_block = "${var.vpc-fullcidr}"
    #### this 2 true values are for use the internal vpc dns resolution
